47544860d72fea52fa60aa5aefd20875c8cfeb9f
wsuits6 <wsuits6@gmail.com>
2026-01-15 00:13:13 +0000

    Fix printf statements in vardatatypes program for correct variable output

--- project2/vardatatypes.c
+++ project2/vardatatypes.c
@@ -6,8 +6,8 @@ int main() {
     char Grade = 'A';
 
     //print out varibales
-    printf("Age:", Age);
-    printf("Grade:", Grade);
+    printf("Age: %d\n", Age);
+    printf("Grade: $d\n", Grade);
 
     return 0;
 }
