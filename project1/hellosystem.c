289b6c46d14d3cfb380c9cea68ec689a0aed6cf9
wsuits6 <wsuits6@gmail.com>
2026-01-15 00:11:18 +0000

    Add initial hello_world and vardatatypes programs with basic output

---
+++ project1/hellosystem.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+
+int main() {
+
+    //printing text out 
+    
+    printf("C is talking to the computer \n");
+    return 0;
+}
---
+++ project2/vardatatypes.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    //declaring variables
+    int Age = 10;
+    char Grade = 'A';
+
+    //print out varibales
+    printf("Age:", Age);
+    printf("Grade:", Grade);
+
+    return 0;
+}
