98f84e29ef5058d095b36bc2d4ec48bc480388ca
wsuits6 <wsuits6@gmail.com>
2026-01-15 00:45:28 +0000

    Add vardatatypes.c file with variable declaration and output

---
+++ project2/vardatatypes.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    //declaring variables
+    int Age = 10;
+    char Grade = 'A';
+
+    //print out varibales
+    printf("Age: %d\n", Age);
+    printf("Grade: %d\n", Grade);
+
+    return 0;
+}
