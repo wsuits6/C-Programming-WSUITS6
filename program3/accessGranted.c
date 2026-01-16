11b599db4a62d93ab9bf03ff7c49c4821b1c5ad1
wsuits6 <wsuits6@gmail.com>
2026-01-16 01:00:28 +0000

    added string compare header file

--- program3/accessGranted.c
+++ program3/accessGranted.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
+#include <string.h>  // Needed for strcmp
 
 int main() {
-
-    //declaring variables
+    // declaring variables
     char username[] = "admin";
 
-    if (username == "admin"){
+    if (strcmp(username, "admin") == 0) {  // Compare string contents
         printf("Access Granted");
     } else {
         printf("Access Denied");
     }
+
     return 0;
 }
