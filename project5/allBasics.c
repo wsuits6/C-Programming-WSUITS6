86aee02383cdc0e55ec5450e374d8ff06618ff56
wsuits6 <wsuits6@gmail.com>
2026-01-19 14:55:50 +0000

    add comment header

---
+++ project5/allBasics.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+//=========================================
+// This program is a mini compilation of all  
+// the Basic Concepts in the C programming 
+// Language . varibals . function. printf's etc
+//==========================================
+
+
+int main(){
+    
+    return 0;
+}
